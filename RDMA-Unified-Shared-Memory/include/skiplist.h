9ee535f69f748bf2794913709a5e30b0c50a773c
xurui <xurui185@gmail.com>
2020-03-29 17:35:58 +0800

    PrintTable() ->value instead of addr. bugs in end_key

--- RDMA-Unified-Shared-Memory/include/skiplist.h
+++ RDMA-Unified-Shared-Memory/include/skiplist.h
@@ -486,7 +486,7 @@ void SkipList<KeyType,DataType>::PrintTable()
     {
 //      nodes.push_back(node->next[0]);
       cout << node->next[0]->keyvalue.first.start_key << " ";
-      cout << node->next[0]->keyvalue.first.end_key << " ";
+      cout << node->next[0]->keyvalue.second.end_key << " ";
       cout << "| ";
       node=node->next[0];
     }
